58e18e5281fb3320cdd40793374de6f826215a74
Arjun <Arjun@code.com>
2022-11-16 08:40:17 +0530

    final commit

--- app.js
+++ app.js
@@ -5,20 +5,19 @@ var outputDiv = document.querySelector("#txt-output");
 
 var serverURL="https://api.funtranslations.com/translate/ferb-latin.json";
 
-function getTranslationURL(input){
+function generatedURL(input){
 return (serverURL+"?"+"text="+input)
 }
 
 function errorHandler(error){
-console.log("error occured "+error);
-alert("something went wrong, try after sometime")
+alert("Please try after someTime")
 }
 
 function clickHandler(){
   
     var inputText=txtInput.value; 
 
-    fetch(getTranslationURL(inputText)) 
+    fetch(generatedURL(inputText)) 
     .then(response=>response.json())
     .then(json=>{
         outputDiv.innerHTML=json.contents.translated
